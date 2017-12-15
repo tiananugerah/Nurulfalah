01a965a3256c14a4d60d71e0197904e599c80b94
ASUS <ASUS@DESKTOP-F2B6SS9>
2017-12-15 13:30:29 +0700

    Tugas

---
+++ src/com/nurulalah/db/model/NURULALAH.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nurulalah.db.model;
+
+/**
+ *
+ * @author THOSHIBA
+ */
+public class NURULALAH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
